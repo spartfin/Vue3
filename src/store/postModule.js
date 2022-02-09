96a15159e7b3931b120297bce6f2eed08b6163a4
AlekseyGO <production@gmail.com>
2022-02-09 16:53:08 +0300

    правки

--- src/store/postModule.js
+++ src/store/postModule.js
@@ -8,18 +8,18 @@ export const postModule = {
         searchQuery: '',
         page: 1,
         limit: 10,
-        totalPages: 0,
+        totalPage: 0,
         sortOptions: [
-            { value: 'title', name: 'По названию' },
-            { value: 'body', name: 'По содержимому' },
+            { value: 'title', name: 'По назавнию' },
+            { value: 'body', name: 'По содержимому' }
         ]
     }),
     getters: {
         sortedPosts(state) {
-            return [...state.posts].sort((post1, post2) => post1[state.selectedSort]?.localeCompare(post2[state.selectedSort]))
+            return [...state.posts].sort((post1, post2) => post1[state.selectedSort]?.localeCompare(post2[state.selectedSort]));
         },
         sortedAndSearchedPosts(state, getters) {
-            return getters.sortedPosts.filter(post => post.title.toLowerCase().includes(state.searchQuery.toLowerCase()))
+            return getters.sortedPosts.filter(post => post.title.toLowerCase().includes(state.searchQuery.toLowerCase()));
         }
     },
     mutations: {
@@ -27,22 +27,22 @@ export const postModule = {
             state.posts = posts;
         },
         setLoading(state, bool) {
-            state.isPostsLoading = bool
-        },
-        setPage(state, page) {
-            state.page = page
+            state.isPostsLoading = bool;
         },
         setSelectedSort(state, selectedSort) {
-            state.selectedSort = selectedSort
-        },
-        setTotalPages(state, totalPages) {
-            state.totalPages = totalPages
+            state.selectedSort = selectedSort;
         },
         setSearchQuery(state, searchQuery) {
-            state.searchQuery = searchQuery
+            state.searchQuery = searchQuery;
+        },
+        setPage(state, page) {
+            state.page = page;
+        },
+        setTotalPage(state, totalPage) {
+            state.totalPage = totalPage;
         },
     },
-    actions: {
+    action: {
         async fetchPosts({ state, commit }) {
             try {
                 commit('setLoading', true);
@@ -52,29 +52,29 @@ export const postModule = {
                         _limit: state.limit
                     }
                 });
-                commit('setTotalPages', Math.ceil(response.headers['x-total-count'] / state.limit))
-                commit('setPosts', response.data)
-            } catch (e) {
-                console.log(e)
+                commit('setTotalPage', Math.ceil(response.headers['x-total-count'] / state.limit));
+                commit('setPosts', response.data);
+            } catch (error) {
+                alert('Ошибка')
             } finally {
                 commit('setLoading', false);
             }
         },
         async loadMorePosts({ state, commit }) {
+            commit('setPage', state.page += 1);
             try {
-                commit('setPage', state.page + 1)
                 const response = await axios.get('https://jsonplaceholder.typicode.com/posts', {
                     params: {
                         _page: state.page,
                         _limit: state.limit
                     }
                 });
-                commit('setTotalPages', Math.ceil(response.headers['x-total-count'] / state.limit))
+                commit('setTotalPage', Math.ceil(response.headers['x-total-count'] / state.limit));
                 commit('setPosts', [...state.posts, ...response.data]);
-            } catch (e) {
-                console.log(e)
+            } catch (error) {
+                alert('Ошибка')
             }
-        }
-    },
-    namespaced: true
+        },
+        namespaced: true
+    }
 }
